7941b7d842dad84188611d40dcb834970ba3bdd0
kaushal <kaushalbhesaniya1212@gmail.com>
2022-02-04 18:07:43 +0530

    first commit

---
+++ firebase/index.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServicesAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+module.exports = admin;
